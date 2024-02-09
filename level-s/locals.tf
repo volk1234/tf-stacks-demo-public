dbce109ba06dc43a6311264e4d43758e953ecbed
Denis Azarov <denis_azarov@epam.com>
2024-02-09 11:07:12 +0100

    rename resources

--- level-s/locals.tf
+++ level-s/locals.tf
@@ -1,8 +1,8 @@
 locals {
   env                   = split("-", var.environment_code)[0]
   bucket_name           = "core-storage-${var.environment_code}"
-  queue_name            = "${var.queue_name}-${local.env}"
-  deadletter_queue_name = "${var.queue_name}-deadletter-${local.env}"
+  queue_name            = "${var.queue_name}-${var.environment_code}"
+  deadletter_queue_name = "${var.queue_name}-deadletter-${var.environment_code}"
   queue_url             = "https://sqs.${var.region}.amazonaws.com/${data.aws_caller_identity.current.account_id}/${local.queue_name}"
   sqs_arn               = "arn:aws:sqs:${var.region}:${data.aws_caller_identity.current.account_id}:${local.queue_name}"
   sns_arn               = "arn:aws:sns:${var.region}:${data.aws_caller_identity.current.account_id}:*"
